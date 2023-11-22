ea567fff40cb58ae2497e714030728505aced2d2
Bigdata acc <as11bs22@gmail.com>
2023-11-22 15:12:41 +0200

    added some styles to card ui

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fas } from "@fortawesome/free-solid-svg-icons";
+import "../app/globals.css";
 
 // Add the imported icons to the library
 library.add(fas);
--- ui/product/productCard.js
+++ ui/product/productCard.js
@@ -1,23 +1,26 @@
 import Image from "next/image";
 import React from "react";
+import classes from "./ProductCard.module.css";
 
-function productCard(props) {
+function ProductCard(props) {
   return (
-    <div>
+    <div
+      className={`${classes.cardContainer} text-blue-600 h-40 w-11/12 lg:w-1/4 md:w-1/3 sm:w-1/2 `}
+    >
       <div className={classes.cardImage}>
         <Image src={props.src} alt={props.alt} />
       </div>
       <h3 className={classes.cardTitle}>{props.cardTitle}</h3>
       <h3 className={classes.cardPrice}>{props.cardPrice}</h3>
       {props.isDiscount && (
-        <>
+        <div>
           {" "}
           <h3 className={classes.oldPrice}>{props.oldPrice}</h3>
           <p className={classes.discount}>{props.discount}</p>
-        </>
+        </div>
       )}
     </div>
   );
 }
 
-export default productCard;
+export default ProductCard;
