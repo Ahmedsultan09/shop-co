9605e043abee6316bbe05abe67bc624543d2d39f
Ahmed Sultan <asultan.dev@gmail.com>
2023-11-28 00:01:04 +0200

    adding first steps in the nav bar

--- layout/header/nav.js
+++ layout/header/nav.js
@@ -1,7 +1,26 @@
 import React from "react";
-
+import classes from "./nav.module.css";
+import Image from "next/image";
+import { Vina_Sans as vina } from "next/font/google";
+import { Noto_Naskh_Arabic as noto } from "next/font/google";
+const vinaSans = vina({
+  weight: "400",
+  subsets: ["latin"],
+});
+const Noto = noto({
+  weight: "400",
+  subsets: ["latin"],
+});
 function Nav() {
-  return <div>nav</div>;
+  return (
+    <nav
+      className={`${classes.nav} w-screen h-16 p-7 flex items-center justify-center`}
+    >
+      <div className={`${classes.logo}  ${vinaSans.className} text-4xl `}>
+        SHOP.CO
+      </div>
+    </nav>
+  );
 }
 
 export default Nav;
--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,21 @@
 import React from "react";
-import SocialMediaIcon from "../ui/social-media-icon";
-import ProductCard from "../ui/product/productCard";
+// import SocialMediaIcon from "../ui/social-media-icon";
+// import ProductCard from "../ui/product/productCard";
+import Nav from "../layout/header/nav";
 
 function Index() {
   return (
-    <div>
-      <SocialMediaIcon linkedIn={true} />
+    <>
+      {/* <SocialMediaIcon linkedIn={true} />
       <ProductCard
         rating={2}
         isDiscount={true}
         discount={50}
         oldPrice={122}
         ratingQuantity={65}
-      />
-    </div>
+  /> */}
+      <Nav />
+    </>
   );
 }
 
