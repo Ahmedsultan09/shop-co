2b8d640aa06504cd403af5d4da0befee2d8a1b52
Bigdata acc <as11bs22@gmail.com>
2023-12-11 16:12:31 +0200

    editied the component

--- layout/header/drop-down-menu.js
+++ layout/header/drop-down-menu.js
@@ -3,70 +3,35 @@ import classes from "./nav.module.css";
 import Link from "next/link";
 
 function DropDownMenu() {
+  const categories = [
+    { href: "/categories/music", label: "Music" },
+    { href: "/categories/men's-fashion", label: "Men's Fashion" },
+    { href: "/categories/women's-fashion", label: "Women's Fashion" },
+    { href: "/categories/supermarket", label: "Supermarket" },
+    { href: "/categories/baby-and-toys", label: "Baby & Toys" },
+    { href: "/categories/home", label: "Home" },
+    { href: "/categories/books", label: "Books" },
+    { href: "/categories/beauty-and-health", label: "Beauty & Health" },
+    { href: "/categories/mobiles", label: "Mobiles" },
+    { href: "/categories/electronics", label: "Electronics" },
+  ];
+
+  const CustomLink = ({ href, label }) => (
+    <Link
+      href={href}
+      className="w-1/2 h-6 p-4 flex justify-start items-center hover:bg-slate-100 rounded-md transition-all"
+    >
+      {label}
+    </Link>
+  );
+
   return (
     <div
-      className={`${classes.dropDown}  absolute flex flex-row flex-wrap w-1/2 left-0 top-11 box-content p-4 h-60 rounded-b-xl shadow-2xl z-10`}
+      className={`${classes.dropDown} absolute flex flex-row flex-wrap w-1/2 left-0 top-11 box-content p-4 h-60 rounded-b-xl shadow-2xl z-10`}
     >
-      <Link
-        href="/categories/music"
-        className="w-1/2 h-6 p-4 flex justify-start items-center hover:bg-slate-100 rounded-md transition-all"
-      >
-        Music
-      </Link>
-      <Link
-        href="/categories/men's-fashion"
-        className="w-1/2 h-6 p-4 flex justify-start items-center hover:bg-slate-100 rounded-md transition-all"
-      >
-        Men's Fashion
-      </Link>
-      <Link
-        href="/categories/women's-fashion"
-        className="w-1/2 h-6 p-4 flex justify-start items-center hover:bg-slate-100 rounded-md transition-all"
-      >
-        Women's Fashion
-      </Link>
-      <Link
-        href="/categories/supermarket"
-        className="w-1/2 h-6 p-4 flex justify-start items-center hover:bg-slate-100 rounded-md transition-all"
-      >
-        SuperMarket
-      </Link>
-      <Link
-        href="/categories/baby-and-toys"
-        className="w-1/2 h-6 p-4 flex justify-start items-center hover:bg-slate-100 rounded-md transition-all"
-      >
-        Baby & Toys
-      </Link>
-      <Link
-        href="/categories/home"
-        className="w-1/2 h-6 p-4 flex justify-start items-center hover:bg-slate-100 rounded-md transition-all"
-      >
-        Home
-      </Link>
-      <Link
-        href="/categories/books"
-        className="w-1/2 h-6 p-4 flex justify-start items-center hover:bg-slate-100 rounded-md transition-all"
-      >
-        Books
-      </Link>
-      <Link
-        href="/categories/beauty-and-health"
-        className="w-1/2 h-6 p-4 flex justify-start items-center hover:bg-slate-100 rounded-md transition-all"
-      >
-        Beauty & Health
-      </Link>
-      <Link
-        href="/categories/mobiles"
-        className="w-1/2 h-6 p-4 flex justify-start items-center hover:bg-slate-100 rounded-md transition-all"
-      >
-        Mobiles
-      </Link>
-      <Link
-        href="/categories/electronics"
-        className="w-1/2 h-6 p-4 flex justify-start items-center hover:bg-slate-100 rounded-md transition-all"
-      >
-        Electronics
-      </Link>
+      {categories.map((category, index) => (
+        <CustomLink key={index} {...category} />
+      ))}
     </div>
   );
 }
