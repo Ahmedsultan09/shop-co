4ba0eaacb222347df71077d2ef7d36e2af844388
Bigdata acc <as11bs22@gmail.com>
2024-02-07 19:17:45 +0200

    edited reviews

--- components/home/testmonials/reviews.js
+++ components/home/testmonials/reviews.js
@@ -1,10 +1,20 @@
 import React from "react";
 import CustomerCard from "./customer-card";
+import { Vina_Sans as vina } from "next/font/google";
+
+const vinaSans = vina({
+  weight: "400",
+  subsets: ["latin"],
+});
 
 function Reviews() {
   return (
     <div className="w-full h-full p-10">
-      <h1>OUR HAPPY CUSTOMERS</h1>
+      <h1
+        className={`${vinaSans.className} w-full flex items-center justify-center text-7xl font-bold py-7`}
+      >
+        OUR HAPPY CUSTOMERS
+      </h1>
       <CustomerCard />
     </div>
   );
